334062b4ea1be88dabac87ef9f4f798101afde73
Giovanni Pachera <giovannipachera@MacBook-Pro-di-Givoski.local>
2024-03-17 20:11:18 +0100

    Added FSM structure, rst and clk synchronization

--- 10727536_10735922.vhd
+++ 10727536_10735922.vhd
@@ -21,8 +21,47 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture behavioral of project_reti_logiche is
-    -- type stati is ();
-begin
-
+    type tipo_stato is (RESET, ASPETTA, PARI, DISPARI); -- PARI: parola, DISPARI: cella vuota
+    signal stato_attuale, stato_prossimo: tipo_stato;
+    signal indice: std_logic_vector(9 downto 0);
+    
+    begin
+        gestione_stato: process(i_clk, i_rst)
+        begin   
+            if i_rst = '1' then 
+                stato_attuale <= RESET;
+            elsif rising_edge(i_clk) then
+                stato_attuale <= stato_prossimo;
+            end if;
+        end process;
+        
+        gestione_transizione: process(stato_attuale)
+        begin
+            if stato_attuale = RESET then
+                if i_rst = '0' then
+                    if i_start = '0' then
+                        stato_prossimo <= ASPETTA;
+                    else
+                        stato_prossimo <= PARI;
+                    end if;
+                else
+                    stato_prossimo <= stato_attuale;
+                end if;
+            elsif stato_attuale = ASPETTA then
+                if i_rst = '0' then
+                    if i_start = '0' then
+                        stato_prossimo <= stato_attuale;
+                    else
+                        stato_prossimo <= PARI;
+                    end if;
+                else
+                    stato_prossimo <= RESET;
+                end if;
+            elsif stato_attuale = PARI then
+                -- cambio segnali
+            elsif stato_attuale = DISPARI then
+                -- cambio segnali
+            end if;
+        end process;
+          
 end behavioral;
-}
