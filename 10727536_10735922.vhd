c83d8f2c952b425eacd790c663ee9b7d839a5c0c
orlando <orlandofrancesco.parise@mail.polimi.it>
2024-03-18 15:40:36 +0100

    created a signal to manage first words equal to 0

--- 10727536_10735922.vhd
+++ 10727536_10735922.vhd
@@ -25,6 +25,7 @@ architecture behavioral of project_reti_logiche is
     signal stato_attuale, stato_prossimo: tipo_stato;
     signal indice: std_logic_vector(9 downto 0);    -- indice di scorrimento nella sequenza
     signal modifica: std_logic;     -- indica la necessità di modificare la parola e di conseguenza la credibilità (quando si incontra uno zero in uno spazio di parola, eccetto NOTA)
+    signal trovato_valore_diverso_da_zero: std_logic;   -- indica se è già stato trovato un valore diverso da 0 nella sequenza di parola, rimane 0 fino a quando non se ne trova uno
     signal o_mem_addr_tmp: std_logic_vector(15 downto 0);   -- segnale non sincronizzato
     signal o_mem_data_tmp: std_logic_vector(7 downto 0);    -- segnale non sincronizzato
     signal done_tmp,  o_mem_en_tmp,  o_mem_we: std_logic; -- segnali non sincronizzati
