504165d62faabe23526c83011259f1b1f776636d
Giovanni <giovanni.pachera@mail.polimi.it>
2024-03-18 18:20:00 +0100

    Some fixes

--- 10727536_10735922.vhd
+++ 10727536_10735922.vhd
@@ -29,7 +29,7 @@ architecture behavioral of project_reti_logiche is
     signal trovato_valore_diverso_da_zero: std_logic;   -- indica se è già stato trovato un valore diverso da 0 nella sequenza di parola, rimane 0 fino a quando non se ne trova uno
     signal o_mem_addr_tmp: std_logic_vector(15 downto 0);   -- segnale non sincronizzato
     signal o_mem_data_tmp: std_logic_vector(7 downto 0);    -- segnale non sincronizzato
-    signal o_done_tmp,  o_mem_en_tmp,  o_mem_we_tmp_tmp: std_logic; -- segnali non sincronizzati
+    signal o_done_tmp,  o_mem_en_tmp,  o_mem_we_tmp: std_logic; -- segnali non sincronizzati
     
     begin
         gestione_stato: process(i_clk, i_rst)
@@ -41,11 +41,11 @@ architecture behavioral of project_reti_logiche is
                 indice <= (others => '0');
                 modifica <= '0';
                 trovato_valore_diverso_da_zero <= '0';
-                o_mem_addr_tmp <= (others => '0');
-                o_mem_data_tmp <= (others => '0');
-                o_done_tmp <= '0';
-                o_mem_en_tmp <= '0';
-                o_mem_we_tmp <= '0';
+                o_mem_addr <= (others => '0');
+                o_mem_data <= (others => '0');
+                o_done <= '0';
+                o_mem_en <= '0';
+                o_mem_we <= '0';
 
             elsif rising_edge(i_clk) then
                 stato_attuale <= stato_prossimo;
@@ -86,7 +86,7 @@ architecture behavioral of project_reti_logiche is
                         o_mem_en_tmp <= '1';
                         o_mem_we_tmp <= '0';
                     end if;
-                else;
+                else
                 end if;
             elsif stato_attuale = ATTESA then
                 if i_rst = '0' then
@@ -113,24 +113,24 @@ architecture behavioral of project_reti_logiche is
                         o_mem_en_tmp <= '1';
                         o_mem_we_tmp <= '0';
                     end if;
-                else;
+                else
                 end if;
                 elsif stato_attuale = PARI then
-                    if valore_trovato_diverso_da_zero = '0' then                                                -- nel caso non siano ancora stati trovati valori diversi da 0 nella sequenza
-                        indice <= std_logic_vector(signed(indice) + 1);                                             -- non modifico il valore in quanto già 0 e passo allo stato successivo
+                    if valore_trovato_diverso_da_zero = '0' then -- nel caso non siano ancora stati trovati valori diversi da 0 nella sequenza
+                        indice <= std_logic_vector(signed(indice) + 1); -- non modifico il valore in quanto già 0 e passo allo stato successivo
                         stato_prossimo <= DISPARI;
-                    else                                                                                                                                 -- nel caso abbiamo già trovato una parola diversa da 0 nella sequenza
-                        if  i_mem_data /= "00000000" then                                                               -- il valore del dato nell'indice è 0                    
+                    else -- nel caso abbiamo già trovato una parola diversa da 0 nella sequenza
+                        if  i_mem_data /= "00000000" then -- il valore del dato nell'indice è 0                    
                             indice <= std_logic_vector(signed(indice) + 1);
                         
                     -- cambio segnali
                 elsif stato_attuale = DISPARI then
-                    if valore_trovato_diverso_da_zero = '0' then                                                -- nel caso non siano ancora stati trovati valori diversi da 0 nella sequenza
-                    indice <= std_logic_vector(signed(indice) + 1);                                                 -- non modifico il valore in quanto già 0 e passo allo stato successivo
+                    if valore_trovato_diverso_da_zero = '0' then -- nel caso non siano ancora stati trovati valori diversi da 0 nella sequenza
+                    indice <= std_logic_vector(signed(indice) + 1); -- non modifico il valore in quanto già 0 e passo allo stato successivo
                         stato_prossimo <= PARI;
                     else                                                                                                                                -- nel caso abbiamo già trovato una parola diversa da 0 nella sequenza
-                    if  i_mem_data /= "00000000" then                                                                  -- il valore del dato nell'indice è 0  
-                        o_mem_data <= "00011111";                                                                                -- si scrive la credibilità a 31
+                    if  i_mem_data /= "00000000" then -- il valore del dato nell'indice è 0  
+                        o_mem_data <= "00011111"; -- si scrive la credibilità a 31
                         indice <= std_logic_vector(signed(indice) + 1);
     
                     -- cambio segnali
