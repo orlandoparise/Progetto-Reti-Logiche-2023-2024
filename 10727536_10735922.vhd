fb2e7442a01208db9989ea89b0620e92ded72f91
Giovanni <giovanni.pachera@mail.polimi.it>
2024-03-18 16:52:04 +0100

    Initialization of signals in reset and implementation of RESET and ATTESA states

--- 10727536_10735922.vhd
+++ 10727536_10735922.vhd
@@ -28,39 +28,91 @@ architecture behavioral of project_reti_logiche is
     signal trovato_valore_diverso_da_zero: std_logic;   -- indica se è già stato trovato un valore diverso da 0 nella sequenza di parola, rimane 0 fino a quando non se ne trova uno
     signal o_mem_addr_tmp: std_logic_vector(15 downto 0);   -- segnale non sincronizzato
     signal o_mem_data_tmp: std_logic_vector(7 downto 0);    -- segnale non sincronizzato
-    signal done_tmp,  o_mem_en_tmp,  o_mem_we: std_logic; -- segnali non sincronizzati
+    signal o_done_tmp,  o_mem_en_tmp,  o_mem_we_tmp_tmp: std_logic; -- segnali non sincronizzati
     
     begin
         gestione_stato: process(i_clk, i_rst)
         begin   
             if i_rst = '1' then 
-                stato_attuale <= RESET;
+                stato_attuale <= RESET;                    
+                
+                -- inizializzazione dei segnali non sincronizzati
+                indice <= (others => '0');
+                modifica <= '0';
+                trovato_valore_diverso_da_zero <= '0';
+                o_mem_addr_tmp <= (others => '0');
+                o_mem_data_tmp <= (others => '0');
+                o_done_tmp <= '0';
+                o_mem_en_tmp <= '0';
+                o_mem_we_tmp <= '0';
+
             elsif rising_edge(i_clk) then
                 stato_attuale <= stato_prossimo;
+
+                o_mem_addr <= o_mem_addr_tmp;
+                o_mem_data <= o_mem_data_tmp;
+                o_done <= o_done_tmp;
+                o_mem_we <= o_mem_we_tmp;
+                o_mem_en <= o_mem_en_tmp;
+
             end if;
         end process;
         
         gestione_transizione: process(stato_attuale)
         begin
             if stato_attuale = RESET then
-                if i_rst = '0' then
+                if i_rst = '0' then 
                     if i_start = '0' then
-                        stato_prossimo <= ATTESA;
+                        stato_prossimo <= ATTESA; -- vado in attesa al clock successivo
+
+                        indice <= (others => '0');
+                        modifica <= '0';
+                        trovato_valore_diverso_da_zero <= '0';
+                        o_mem_addr_tmp <= (others => '0');
+                        o_mem_data_tmp <= (others => '0');
+                        o_done_tmp <= '0';
+                        o_mem_en_tmp <= '0';
+                        o_mem_we_tmp <= '0';
                     else
-                        stato_prossimo <= PARI;
+                        stato_prossimo <= PARI; -- inizio la computazione al clock successivo
+
+                        indice <= (others => '0');
+                        modifica <= '0';
+                        trovato_valore_diverso_da_zero <= '0';
+                        o_mem_addr_tmp <= (others => '0');
+                        o_mem_data_tmp <= (others => '0');
+                        o_done_tmp <= '0';
+                        o_mem_en_tmp <= '1';
+                        o_mem_we_tmp <= '0';
                     end if;
-                else
-                    stato_prossimo <= stato_attuale;
+                else;
                 end if;
             elsif stato_attuale = ATTESA then
                 if i_rst = '0' then
                     if i_start = '0' then
                         stato_prossimo <= stato_attuale;
+
+                        indice <= (others => '0');
+                        modifica <= '0';
+                        trovato_valore_diverso_da_zero <= '0';
+                        o_mem_addr_tmp <= (others => '0');
+                        o_mem_data_tmp <= (others => '0');
+                        o_done_tmp <= '0';
+                        o_mem_en_tmp <= '0';
+                        o_mem_we_tmp <= '0';
                     else
                         stato_prossimo <= PARI;
+
+                        indice <= (others => '0');
+                        modifica <= '0';
+                        trovato_valore_diverso_da_zero <= '0';
+                        o_mem_addr_tmp <= (others => '0');
+                        o_mem_data_tmp <= (others => '0');
+                        o_done_tmp <= '0';
+                        o_mem_en_tmp <= '1';
+                        o_mem_we_tmp <= '0';
                     end if;
-                else
-                    stato_prossimo <= RESET;
+                else;
                 end if;
             elsif stato_attuale = PARI then
                 -- cambio segnali
