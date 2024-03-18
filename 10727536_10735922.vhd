b4f2ae27a2d16d0604449c37b0a199b6e6785b2c
orlando <orlandofrancesco.parise@mail.polimi.it>
2024-03-18 12:20:51 +0100

    added tmp signals and micro changes

--- 10727536_10735922.vhd
+++ 10727536_10735922.vhd
@@ -21,9 +21,13 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture behavioral of project_reti_logiche is
-    type tipo_stato is (RESET, ASPETTA, PARI, DISPARI); -- PARI: parola, DISPARI: cella vuota
+    type tipo_stato is (RESET, ATTESA, PARI, DISPARI);  -- PARI: parola, DISPARI: cella vuota
     signal stato_attuale, stato_prossimo: tipo_stato;
-    signal indice: std_logic_vector(9 downto 0);
+    signal indice: std_logic_vector(9 downto 0);    -- indice di scorrimento nella sequenza
+    signal modifica: std_logic;     -- indica la necessità di modificare la parola e di conseguenza la credibilità (quando si incontra uno zero in uno spazio di parola, eccetto NOTA)
+    signal o_mem_addr_tmp: std_logic_vector(15 downto 0);   -- segnale non sincronizzato
+    signal o_mem_data_tmp: std_logic_vector(7 downto 0);    -- segnale non sincronizzato
+    signal done_tmp,  o_mem_en_tmp,  o_mem_we: std_logic; -- segnali non sincronizzati
     
     begin
         gestione_stato: process(i_clk, i_rst)
@@ -40,14 +44,14 @@ architecture behavioral of project_reti_logiche is
             if stato_attuale = RESET then
                 if i_rst = '0' then
                     if i_start = '0' then
-                        stato_prossimo <= ASPETTA;
+                        stato_prossimo <= ATTESA;
                     else
                         stato_prossimo <= PARI;
                     end if;
                 else
                     stato_prossimo <= stato_attuale;
                 end if;
-            elsif stato_attuale = ASPETTA then
+            elsif stato_attuale = ATTESA then
                 if i_rst = '0' then
                     if i_start = '0' then
                         stato_prossimo <= stato_attuale;
