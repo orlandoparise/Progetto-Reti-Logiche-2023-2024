676b7aa0cbbf4d116b220101042b7bcfcae42618
orlandoparise <orlandofrancesco.parise@mail.polimi.it>
2024-03-30 00:06:04 +0100

    small correction

--- Testbenches/project_tb_k_zero.vhd
+++ Testbenches/project_tb_k_zero.vhd
@@ -5,10 +5,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use std.textio.all;
 
-entity project_tb is
-end project_tb;
+entity project_tb_k is
+end project_tb_k;
 
-architecture project_tb_arch of project_tb is
+architecture project_tb_k_arch of project_tb_k is
     constant CLOCK_PERIOD : time := 20 ns;
     signal tb_clk : std_logic := '0';
     signal tb_rst, tb_start, tb_done : std_logic;
